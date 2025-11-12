119590e48dbfbdcd43a180c5ddcf29ff3582b624
alaba <addall14991@proton.me>
2025-11-12 21:07:30 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {join} from "path";
+
+const app = express();
+const __dirname = import.meta.dirname;
+
+app.use(express.static(join(__dirname, "public")));
+
+app.get("/", (_, res) => {
+    error.sendFile(join(__dirname, "public", "index.html"));
+});
+
+app.listen(4000, err => err ? console.error(err): console.log("Working..."));
